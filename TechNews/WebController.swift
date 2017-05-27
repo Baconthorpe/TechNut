da9f99974b2e3becaccd60f2ec9215ca509439c5
Ezekiel Abuhoff <ezekielabuhoff@Ezekiels-MacBook-Pro.local>
2017-05-26 17:45:36 -0700

    added open browser button to web controller

--- TechNews/WebController.swift
+++ TechNews/WebController.swift
@@ -13,6 +13,11 @@ class WebController: UIViewController {
     // MARK: UI Elements
     @IBOutlet weak var webView: UIWebView!
     
+    // MARK: UI Actions
+    @IBAction func openInBrowserTapped(_ sender: Any) {
+        openPageInBrowser()
+    }
+    
     // MARK: Passed-In Data
     var url: String?
     
@@ -28,5 +33,19 @@ class WebController: UIViewController {
         
         let request = URLRequest(url: validURL)
         webView.loadRequest(request)
+    }
+    
+    // MARK: Opening Browser
+    internal func openPageInBrowser() {
+        guard let presentURL = url else { return }
+        guard let validURL = URL(string: presentURL) else { return }
+        
+        UIApplication.shared.open(validURL, options: [:]) { (success) in
+            if success {
+                print("Successfully opened URL: \(validURL)")
+            } else {
+                print("Failed to open URL: \(validURL)")
+            }
+        }
     }
 }
